da9b3cf91eb70ae83faf48ec601b0572a204c4d2
shuninamarina <marinashunina03@gmail.com>
2022-12-27 21:06:42 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,10 @@
-const categoriesElem = document.querySelector("#categories.item");
+const categoriesElem = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categoriesElem.length}`);
+
+categoriesElem.forEach((category) => {
+  const titleElem = category.querySelector("h2");
+  const listElem = titleElem.nextElementSibling.children;
+
+  console.log(`Category: ${titleElem.textContent}`);
+  console.log(`Elements: ${listElem.length}`);
+});
