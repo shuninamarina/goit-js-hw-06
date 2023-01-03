08204cef058c5adf03615840b0525fde4845a912
shuninamarina <marinashunina03@gmail.com>
2023-01-03 15:18:34 +0200

    upd

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredient = document.querySelector("#ingredients");
+
+const createElem = ingredients.map((elem) => {
+  const ingredientElem = document.createElement("li");
+  ingredientElem.classList.add("item");
+  ingredientElem.textContent = elem;
+
+  return ingredientElem;
+});
+
+ingredient.append(...createElem);
