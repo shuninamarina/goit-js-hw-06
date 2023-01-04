1f6532ea5e358c50f1d635da95207d97f06766ea
shuninamarina <marinashunina03@gmail.com>
2023-01-04 14:11:26 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyBackground = document.querySelector("body");
+const changeBtn = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+
+changeBtn.addEventListener("click", () => {
+  bodyBackground.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = bodyBackground.style.backgroundColor;
+});
