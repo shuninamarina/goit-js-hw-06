3ac972588cdedf76235db93bd16d35aac7362e8b
shuninamarina <marinashunina03@gmail.com>
2023-01-04 14:02:30 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", submitBtn);
+
+function submitBtn(event) {
+  event.preventDefault();
+
+  const elements = {
+    email: event.currentTarget.email.value,
+    password: event.currentTarget.password.value,
+  };
+
+  const { email, password } = elements;
+
+  if (email === "" || password === "") {
+    return alert("Всі поля повинні бути заповнені!");
+  }
+
+  console.log(elements);
+  event.currentTarget.reset();
+}
